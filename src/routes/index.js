c5c1e940cff5e57f8c8fa49eda17fa0a2ea7b5be
jeet1desai <jeetdesai995@gmail.com>
2021-12-25 19:18:07 +0530

    4th commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,10 @@ import express from 'express';
 const indexRouter = express.Router();
 
 import { indexPage, messagesPage, addMessage } from '../controllers';
+import { modifyMessage } from '../middleware';
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/messages', messagesPage);
-indexRouter.post('/messages', addMessage);
+indexRouter.post('/messages', modifyMessage, addMessage);
 
 export default indexRouter;
