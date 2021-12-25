e4d0aea727852b44b4b16bf83f769c07e2a7742b
jeet1desai <jeetdesai995@gmail.com>
2021-12-25 19:04:50 +0530

    3rd commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { testEnvironmentVariable } from '../settings';
-
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) =>
-  res.status(200).json({ message: testEnvironmentVariable })
-);
+import { indexPage, messagesPage, addMessage } from '../controllers';
+
+indexRouter.get('/', indexPage);
+indexRouter.get('/messages', messagesPage);
+indexRouter.post('/messages', addMessage);
 
 export default indexRouter;
