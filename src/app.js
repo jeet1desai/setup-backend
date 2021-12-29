04fb5e969a5b88ef92781edc2c7547545ae79935
jeet1desai <jeetdesai995@gmail.com>
2021-12-29 10:22:34 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -6,6 +6,13 @@ import indexRouter from './routes/index';
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE, FETCH');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
